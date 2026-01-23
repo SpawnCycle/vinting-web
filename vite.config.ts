de0993a930758cf34b59d656227c90a96d73a5f3
SpawnCycle <joloman386@gmail.com>
2026-01-23 08:43:20 +0100

    conditional outDir

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+import { existsSync } from "fs";
+
+const outDir = existsSync("../Cargo.toml") ? "../web/" : "build/";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir,
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
